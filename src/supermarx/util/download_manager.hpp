913714a1393abae0cfdae49c7375b193b658aeb5
Wouter Geraedts <git@woutergeraedts.nl>
2015-12-10 16:55:42 +0100

    Added error_f to download_manager

--- src/supermarx/util/download_manager.hpp
+++ src/supermarx/util/download_manager.hpp
@@ -13,6 +13,7 @@ class download_manager
 {
 public:
 	typedef std::function<void(downloader::response)> f_t;
+	typedef std::function<void()> error_f_t;
 
 	struct job_t
 	{
@@ -26,16 +27,20 @@ public:
 private:
 	cached_downloader& dl;
 	std::deque<job_t> queue;
+	error_f_t error_f;
+
+	static void nil() {}
 
 public:
-	download_manager(cached_downloader& _dl)
+	download_manager(cached_downloader& _dl, error_f_t const& _error_f = nil)
 		: dl(_dl)
 		, queue()
+		, error_f(_error_f)
 	{}
 
-	void schedule(std::string&& _uri, f_t&& _f)
+	void schedule(std::string const& _uri, f_t&& _f)
 	{
-		job_t j = {std::move(_uri), std::move(_f)};
+		job_t j = {_uri, std::move(_f)};
 		queue.emplace_back(std::move(j));
 	}
 
@@ -46,25 +51,17 @@ public:
 			job_t j = std::move(queue.front());
 			queue.pop_front();
 
-			downloader::response response;
 			try
 			{
-				response = std::move(stubborn::attempt<downloader::response, downloader::error>([&]() {
+				j.f(stubborn::attempt<downloader::response, downloader::error>([&]() {
 					return dl.fetch(j.uri);
 				}));
-			} catch(downloader::error e)
-			{
-				std::cerr << "Failed " << j.uri << std::endl
-					<< '\t' << e.what() << std::endl;
-			}
-
-			try
-			{
-				j.f(response);
 			} catch(std::runtime_error e)
 			{
 				std::cerr << "Failed " << j.uri << std::endl
 					<< '\t' << e.what() << std::endl;
+				error_f();
+				continue;
 			}
 		}
 	}
